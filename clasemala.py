375fa0813a9ee7d1705f147946489766829d5f44
Ignacio Gutierrez <ig.gutierrezh@duocuc.cl>
2024-06-14 13:19:45 -0400

    menú listo

---
+++ clasemala.py
@@ -0,0 +1,23 @@
+import os
+import time
+
+while True:
+    print("Menú Trabajadores")
+    print("1. Registrar trabajador")
+    print("2. Listar trabajadores")
+    print("3. Imprimir planilla de sueldos")
+    print("4. Salir del programa")
+
+    opc = int(input("Ingrese opción: "))
+    os.system("cls")
+
+    if opc == 1:
+        pass
+    elif opc == 2:
+        pass
+    elif opc == 3:
+        pass
+    else:
+        print("Muchas gracias, adios!")
+        break
+    time.sleep(3) 
